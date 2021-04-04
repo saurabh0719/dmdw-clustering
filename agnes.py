7d60e191112021d969302d49538ed2ad9adf5e80
Saurabh Pujari <saurabhpuj99@gmail.com>
2021-04-04 14:15:22 +0530

    Agglomerative Nesting

--- agnes.py
+++ agnes.py
@@ -10,5 +10,30 @@ from sklearn.cluster import AgglomerativeClustering
 import seaborn as sns
 
 
+def buildClusters():
+    dataframe = pd.read_csv("Mall_Customers.csv")
+    x = dataframe.iloc[:, [3,4]].values # Extract 2 specified columns
+    dist = linkage(x, "ward")
+    dataset_new = dataframe.copy()
+    dataset_new['3_clusters'] = fcluster(dist, 3, criterion="maxclust")
+    sns.scatterplot(x="Annual Income (k$)", y="Spending Score (1-100)", data=dataset_new, hue="3_clusters")
+    plt.title("AGNES clusters")
+    plt.xlabel("Annual Income")
+    plt.ylabel("Spending score")
+    plt.legend()
+    plt.show()
+
 def Dendrogram(): 
-    
+    dataframe = pd.read_csv("Mall_Customers.csv")
+    x = dataframe.iloc[:, [3,4]].values # Extract 2 specified columns
+    link = linkage(x, "ward") # calculate nearest point distance 
+    dendrogram(link, orientation="top", distance_sort="descending", show_leaf_counts=True)
+    plt.title("Dendogram")
+    plt.show()
+    buildClusters()
+
+Dendrogram()
+
+"""
+Dendogram shows us how many optimal number of clusters should be formed
+"""
