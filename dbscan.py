40e35168bf80a8d615864f2a7cb02c28a4e3f444
Saurabh Pujari <saurabhpuj99@gmail.com>
2021-04-04 23:06:28 +0530

    clean up

--- dbscan.py
+++ dbscan.py
@@ -8,18 +8,6 @@ dataframe = pd.read_csv("Mall_Customers.csv")
 x = dataframe.iloc[:, [3,4]].values # Extract 2 specified columns 
 dbscan = DBSCAN(eps=5.5, min_samples=4)
 labels=dbscan.fit_predict(x)
-# dbscan.fit(x)
-
-# pca = PCA(n_components=2).fit(x)
-# pca_2d = pca.transform(x)
-# print(pca_2d.shape[0])
-# for i in range(0, pca_2d.shape[0]):
-#     if dbscan.labels_[i] == 0:
-#         c1 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='r',marker='+')
-#     elif dbscan.labels_[i] == 1:
-#         c2 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='g',marker='o')
-#     elif dbscan.labels_[i] == -1:
-#         c3 = plt.scatter(pca_2d[i,0],pca_2d[i,1],c='b', marker='*')
 
 print(np.unique(labels))
 
@@ -32,8 +20,6 @@ plt.scatter(x[labels==4,0], x[labels==4,1], s=100, c='pink')
 plt.scatter(x[labels==5,0], x[labels==5,1], s=100, c='cyan')
 
 
-
-# plt.legend([c1, c2, c3, c4], ['Cluster 1', 'Cluster 2', 'Cluster 3','Noise'])
 plt.title('DBSCAN')
 plt.xlabel("Annual Income")
 plt.ylabel("Spending score")
